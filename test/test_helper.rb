fc059c917e734fcd987c1bbf110f4493272b84c1
zhitongLIU <zhitonggm.liu@gmail.com>
2017-08-02 23:43:51 +0200

    Add test for index view

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/unit'
 require 'mocha/mini_test'
+require 'capybara/rails'
+require 'capybara/minitest'
 
 # To add Capybara feature tests add `gem 'minitest-rails-capybara'`
 # to the test group in the Gemfile and uncomment the following:
@@ -19,3 +21,18 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  # Make `assert_*` methods behave like Minitest assertions
+  include Capybara::Minitest::Assertions
+
+  # Reset sessions and driver between tests
+  # Use super wherever this method is redefined in your individual test classes
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
+
